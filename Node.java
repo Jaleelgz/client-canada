8f5850b93b6d57a75805625e6f027fee6516c851
Jaleelgz <abduljaleelkolamban@gmail.com>
2021-04-25 06:15:05 +0530

    sum of digits successful

---
+++ Node.java
@@ -0,0 +1,9 @@
+public class Node{    
+        double data;    
+        MyNode next;    
+            
+        public MyNode(double data) {    
+            this.data = data;    
+            this.next = null;    
+        }    
+    }   
---
+++ SumOfDigits.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class SumOfDigits{
+	int sum=0;
+	public static void main(String args[])
+	{
+		int num=0;
+		while(num==0)
+		{
+		try
+		{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter an integer number:");
+		num=sc.nextInt();
+		}
+		catch(Exception e)
+		{
+		System.out.println("Input Error!");
+		}
+	}
+		SumOfDigits obj=new SumOfDigits();
+		int result=obj.sumf(num);
+		System.out.println("Result: "+result);
+		System.out.println("End of Processing");
+		}
+	int sumf(int num)
+	{
+		sum=num%10;
+		if(num==0)
+		{
+			return 0;
+		}
+		else{
+			return sum+sumf(num/10);
+		}
+	}
+		
+		
+}
